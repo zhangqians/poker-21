444c4889afba484b61806752144098b069f2987a
zhagnian <zhagnian@baidu.com>
2016-07-30 11:13:33 +0800

    zhangqian

--- spec/poker-spec.js
+++ spec/poker-spec.js
@@ -2,7 +2,7 @@
  * Created by zhagnian on 16-7-29.
  */
 'use strict';
-let {formatInputs, getNormalPoints, getAllPoints, comparePoints, formatInput_s, getNormalPoint_s, getAllPoint_s, poker}=require('../src/poker.js');
+let {formatInputs, getNormalPoints, getAllPoints, comparePoints,poker}=require('../src/poker.js');
 
 
 describe('poker.js', function () {
@@ -39,7 +39,7 @@ describe('poker.js', function () {
 
     it('should print format points inputs', ()=> {
         let inputs = "3-A-J-1-A";
-        let builtFormatPoints = formatInput_s(inputs);
+        let builtFormatPoints = formatInputs(inputs);
         let printFormation = ['3', 'A', 'J', '1', 'A'];
         expect(builtFormatPoints).toEqual(printFormation);
 
@@ -47,7 +47,7 @@ describe('poker.js', function () {
 
     it('should print normal points', ()=> {
         let inputs = ['3', 'A', 'J', '1', 'A'];
-        let gotNormalPoints = getNormalPoint_s(inputs);
+        let gotNormalPoints = getNormalPoints(inputs);
         let printNormalPoints = 14;
         expect(gotNormalPoints).toEqual(printNormalPoints);
     });
@@ -55,7 +55,7 @@ describe('poker.js', function () {
     it('should print all points', ()=> {
         let inputs = 22;
         let input = ['3', '4', '5', 'A', 'J'];
-        let allPoints = getAllPoint_s(inputs, input);
+        let allPoints = getAllPoints(inputs, input);
         let builtAllPoints = 23;
         expect(allPoints).toEqual(builtAllPoints);
     });
@@ -64,7 +64,7 @@ describe('poker.js', function () {
     it('should print all points', ()=> {
         let inputs = 14;
         let input = ['3', 'A', 'J', '1', 'A'];
-        let allPoints = getAllPoint_s(inputs, input);
+        let allPoints = getAllPoints(inputs, input);
         let builtAllPoints = 16;
         expect(allPoints).toEqual(builtAllPoints);
     });
@@ -130,6 +130,16 @@ describe('poker.js', function () {
         let input = "Q-3-J-A";
         let getWinner = poker(inputs, input);
         let printWinner = "both loser";
+        expect(getWinner).toEqual(printWinner);
+
+    });
+
+
+    it('should print the winner', ()=> {
+        let inputs = "J-3-K-A";
+        let input = "1-3-1-A";
+        let getWinner = poker(inputs, input);
+        let printWinner = "B is winner";
         expect(getWinner).toEqual(printWinner);
 
     });
--- src/poker.js
+++ src/poker.js
@@ -13,18 +13,9 @@ function formatInputs(tags) {
     return a;
 
 }
-function formatInput_s(tag) {
-    let a = _.chain(tag)
-        .split('-')
-        .value();
-    console.log(a);
-    return a;
-
-}
 function getNormalPoints(getPoints) {
     return _.chain(getPoints)
         .filter(x=>x !== 'A')
-        //.map(x=>_.replace)
         .map(x=> {
             //console.log(x);
             if (x === "J" || x === "Q" || x === "K") {
@@ -38,23 +29,7 @@ function getNormalPoints(getPoints) {
 //console.log(a);
 
 }
-function getNormalPoint_s(getPoint) {
-    return _.chain(getPoint)
-        .filter(x=>x !== 'A')
-        //.map(x=>_.replace)
-        .map(x=> {
-            //console.log(x);
-            if (x === "J" || x === "Q" || x === "K") {
-                return _.replace(x, x, "10");
-            } else
-                return x;
-        })
-        .map(x=>_.parseInt(x))
-        .sum()
-        .value();
-//console.log(a);
 
-}
 function getAllPoints(gotNormalPoints, getPoints) {
     let add = gotNormalPoints;
     //console.log(add);
@@ -85,36 +60,7 @@ function getAllPoints(gotNormalPoints, getPoints) {
     return sumA;
 
 }
-function getAllPoint_s(gotNormalPoint, getPoint) {
-    let add = gotNormalPoint;
-    console.log(add);
-    let sumB;
-    let a = _.chain(getPoint)
-        .filter(x=>x === 'A')
-        .value();
-    if (a.length === 0) {
-        sumB = add
-    }
-    let sub = 21 - add;
-    if (a.length > 0 && sub < 0) {
-        sumB = add + a.length;
-    } else if (a.length > 0 && sub > 0) {
-        let div = sub / 11;
-        //console.log(div);
-        if (div < 1) {
-            sumB = add + a.length;
-        } else if (div > 1 && a.length === 1) {
-            sumB = add + 11;
-        } else if (div > 1 && a.length > 1) {
-            sumB = add + 11 + a.length - 1;
-        }
 
-    }
-
-    console.log(sumB);
-    return sumB;
-
-}
 function comparePoints(gotAllPoints, gotAllPoint, getPoints, getPoint) {
     if (gotAllPoints > 21 && gotAllPoint > 21) {
         console.log('both loser');
@@ -148,9 +94,7 @@ function comparePoints(gotAllPoints, gotAllPoint, getPoints, getPoint) {
             console.log('A is winner');
             return 'A is winner';
         }
-
     }
-
 }
 
 function poker(tags, tag) {
@@ -159,9 +103,9 @@ function poker(tags, tag) {
     let gotAllPoints = getAllPoints(gotNormalPoints, getPoints);
 
 
-    let getPoint = formatInput_s(tag);
-    let gotNormalPoint = getNormalPoint_s(getPoint);
-    let gotAllPoint = getAllPoint_s(gotNormalPoint, getPoint);
+    let getPoint = formatInputs(tag);
+    let gotNormalPoint = getNormalPoints(getPoint);
+    let gotAllPoint = getAllPoints(gotNormalPoint, getPoint);
 
 
     let note = comparePoints(gotAllPoints, gotAllPoint, getPoints, getPoint);
@@ -174,10 +118,6 @@ module.exports = {
     formatInputs: formatInputs,
     getNormalPoints: getNormalPoints,
     getAllPoints: getAllPoints,
-    formatInput_s: formatInput_s,
-    getNormalPoint_s: getNormalPoint_s,
-    getAllPoint_s: getAllPoint_s,
-
     comparePoints: comparePoints,
     poker: poker
 };
